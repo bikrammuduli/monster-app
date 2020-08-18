fa635c9e4cac10f98eaef2b48f7e3f0a71a88de2
digientropy <bikrammuduli1@gmail.com>
2020-08-18 18:48:25 +0530

    Updates

---
+++ precache-manifest.324f30295107938677350753c6f0a9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7220d8403eef3b2aa410445d178299",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "fc257dd39241a9106a33",
+    "url": "/monster-app/static/css/main.0e267212.chunk.css"
+  },
+  {
+    "revision": "24a566501b91b3e3852a",
+    "url": "/monster-app/static/js/2.6759b955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.6759b955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc257dd39241a9106a33",
+    "url": "/monster-app/static/js/main.b6d99dae.chunk.js"
+  },
+  {
+    "revision": "70ffa8869a950765e1c3",
+    "url": "/monster-app/static/js/runtime-main.4edd5f74.js"
+  }
+]);
